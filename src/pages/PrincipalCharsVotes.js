946154445e69a038c1b19ac72d04ef6c3f7009ae
Anartz Mugika Ledo <mugan86@gmail.com>
2021-12-24 18:45:10 +0100

    Implement Bar Chart with characters votes count

--- src/pages/PrincipalCharsVotes.js
+++ src/pages/PrincipalCharsVotes.js
@@ -1,22 +1,47 @@
 import { useQuery } from "@apollo/client";
 import { PRINCIPAL_CHARACTERS_VOTES } from "../graphql/operations/query";
 import Loading from "../components/Loading";
+import BarChart from "../components/BarChart";
 import Button from "../components/Button";
 import PageTitle from "../components/PageTitle";
 const PrincipalCharsVotes = () => {
   const { loading, error, data } = useQuery(PRINCIPAL_CHARACTERS_VOTES);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Loading />;
   if (error) return <p>Error :(</p>;
   const { characters } = data;
+  let labels = [];
+  let values = [];
+  if (!loading) {
+    characters.map((character) => {
+      labels.push(character["name"]);
+      values.push(character["votes"]);
+      return "";
+    });
+  }
 
   return (
     <>
-    <PageTitle title={'Principal Characters Votes'} />
-      {loading && <Loading />}
+      <PageTitle title={"Principal Characters Votes"} />
       {!loading && (
-        <div className="row">
-          <div className="col-9">
+        <>
+          <div className="row">
+            <div className="col-9">
+              <BarChart
+                titleChart={"Vote count bar chart"}
+                labels={labels}
+                dataValues={values}
+              />
+            </div>
+            <div className="col d-grid gap-2">
+              {characters.map((character, index) => (
+                <Button key={index} label={character.name} />
+              ))}
+            </div>
+          </div>
+          <br />
+          <br />
+          <div className="row">
             <div className="table-responsive">
               <table className="table">
                 <thead>
@@ -32,31 +57,26 @@ const PrincipalCharsVotes = () => {
                 <tbody>
                   {characters.map((character, index) => (
                     <tr key={index}>
-                      <td>{character['name']}</td>
-                      <td>{character['actor']}</td>
+                      <td>{character["name"]}</td>
+                      <td>{character["actor"]}</td>
                       <td>
                         <a
-                          href={character['url']}
+                          href={character["url"]}
                           target="_blank"
                           rel="noreferrer"
                         >
                           Wikipedia
                         </a>
                       </td>
-                      <td>{character['total_episodes']}</td>
-                      <td>{character['votes']}</td>
+                      <td>{character["total_episodes"]}</td>
+                      <td>{character["votes"]}</td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
           </div>
-          <div className="col d-grid gap-2">
-            {characters.map((character, index) => (
-              <Button key={index} label={character.name} />
-            ))}
-          </div>
-        </div>
+        </>
       )}
     </>
   );
