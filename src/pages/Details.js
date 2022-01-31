86c5e1405d4e2608ebbb777c906fc6121e112076
Anartz Mugika Ledo <mugan86@gmail.com>
2022-01-31 19:24:17 +0100

    fix: i18n not apply correct in 'Details'. Resolve

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -8,7 +8,7 @@ import { CHANGE_VOTE_LISTENER } from "../graphql/operations/subscription";
 import { Loading, PageTitle, Button } from "../components";
 import { openExternalURL } from "../helpers/navigate";
 const Details = () => {
-  const { t } = useTranslation();
+  const { t } = useTranslation("details");
   const [idProduct] = useState(useParams().id);
   const [character, setCharacter] = useState({});
   const { loading, error, data } = useQuery(GET_CHARACTER, {
@@ -58,18 +58,18 @@ const Details = () => {
               />
             </div>
             <div className="col-lg">
-              <h5 className="my-3">{character.votes} {t("pages.details.votes")}</h5>
+              <h5 className="my-3">{character.votes} {t("votes")}</h5>
               <p>{character.description}</p>
               <Button
                 key={character.id}
-                label={`${t("pages.details.vote_lbl")} ${character.name}`}
+                label={`${t("vote_lbl")} ${character.name}`}
                 param={character.id}
                 action={voteCharacter}
               />
               &nbsp;
               <Button
                 key={`Wiki_${character.id}`}
-                label={t("pages.details.more_info")}
+                label={t("more_info")}
                 param={character.url}
                 action={goToMoreDetails}
               />
