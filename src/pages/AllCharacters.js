53d615bc0a9ff45e6682322a0bae7a23bd935fb0
Ruslan Gonzalez <ruslangb@thenetworkcontrolgroup.com>
2022-02-02 19:59:55 +0000

    feature: add swr functionality for characters view

--- src/pages/AllCharacters.js
+++ src/pages/AllCharacters.js
@@ -1,22 +1,29 @@
-import { useFetchApi } from "../hooks/useFetchApi";
-import { API_URL_BASE, API_ENDPOINTS } from "../helpers/urls";
 import Card from "../components/Card";
 import Loading from "../components/Loading";
 import PageTitle from "../components/PageTitle";
 import { useTranslation } from "react-i18next";
+import useSWR from "swr";
+import { fetcher } from "../helpers/fetcher";
+import { API_ENDPOINTS, API_URL_BASE } from "../helpers/urls";
 const AllCharacters = () => {
   const { t } = useTranslation("characters");
-  const { loading, data } = useFetchApi(
-    `${API_URL_BASE}${API_ENDPOINTS.CHARACTERS}`
+  const { data } = useSWR(
+    `${API_URL_BASE}${API_ENDPOINTS.CHARACTERS}`,
+    fetcher
   );
+
   return (
     <>
       <PageTitle title={t("title")} />
-      {loading && <Loading />}
-      {!loading && (
+      {!data && <Loading />}
+      {data && (
         <div className="row row-cols-1 row-cols-md-3 g-4">
           {data.map((character, index) => (
-            <Card character={character} useLabel={t("appearance_seasons")} key={index} />
+            <Card
+              character={character}
+              useLabel={t("appearance_seasons")}
+              key={index}
+            />
           ))}
         </div>
       )}
